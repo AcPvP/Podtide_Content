81e00d9c86224595b3b77a9593afd756b6266788
Mike <66185485+PotatoNPK@users.noreply.github.com>
2021-12-18 12:40:05 -0800

    timer updates
    change stipend and aug gem timer from 24 hours to 20 hours.

--- sql/quests/augmentationblankgemacquired.sql
+++ sql/quests/augmentationblankgemacquired.sql
@@ -1,4 +1,4 @@
 DELETE FROM `quest` WHERE `name` = 'AugmentationBlankGemAcquired';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('AugmentationBlankGemAcquired', 86400, -1, 'Blank Aug Gem Pickup Timer', '2021-11-01 00:00:00');
+VALUES ('AugmentationBlankGemAcquired', 72000, -1, 'Blank Aug Gem Pickup Timer', '2021-11-01 00:00:00');
