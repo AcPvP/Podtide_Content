e203bf0d53f07aa45e1981618c1dd01d06459000
Mike <66185485+PotatoNPK@users.noreply.github.com>
2021-12-31 11:19:18 -0800

    Update 1028707 Dalomar.sql
    re-add WCID's that got nuked

--- sql/weenies/PK Trophy Vendor/1028707 Dalomar.sql
+++ sql/weenies/PK Trophy Vendor/1028707 Dalomar.sql
@@ -423,7 +423,15 @@ VALUES (1028707, 2,    74,  0, 92, 0, False) /* Create Heaume (74) for Wield */
      , (1028707, 4, 528869,  0, 0, 0, False) /* Create  (528869) for Shop */
      , (1028707, 4, 532168,  0, 0, 0, False) /* Create  (532168) for Shop */
      , (1028707, 4, 545031,  0, 0, 0, False) /* Create  (545031) for Shop */
-     , (1028707, 4, 2031668,  0, 0, 0, False) /* Create  (2031668) for Shop */;
+     , (1028707, 4, 2031668,  0, 0, 0, False) /* Create  (2031668) for Shop */
+     , (1028707, 4, 1006797,  0, 0, 0, False) /* Create  (1006797) for Shop */
+     , (1028707, 4, 1006800,  0, 0, 0, False) /* Create  (1006800) for Shop */
+     , (1028707, 4, 1006802,  0, 0, 0, False) /* Create  (1006802) for Shop */
+     , (1028707, 4, 1006804,  0, 0, 0, False) /* Create  (1006804) for Shop */
+     , (1028707, 4, 42149813,  0, 0, 0, False) /* Create  (42149813) for Shop */
+     , (1028707, 4, 1910618,  0, 0, 0, False) /* Create  (1910618) for Shop */
+     , (1028707, 4, 1910619,  0, 0, 0, False) /* Create  (1910619) for Shop */
+     , (1028707, 4, 42130741,  0, 0, 0, False) /* Create  (42130741) for Shop */;
 
 /* Lifestoned Changelog:
 {
