71fb236f843b69ea1cfd1640e97d766b222b2690
Mike <66185485+PotatoNPK@users.noreply.github.com>
2022-01-19 18:23:17 -0800

    Create 4200012 Burgler Tools.sql
    add new version of skelly key that opens absolutely anything that is locked.

---
+++ sql/weenies/Town Control/4200012 Burgler Tools.sql
@@ -0,0 +1,34 @@
+DELETE FROM `weenie` WHERE `class_Id` = 4200012;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (4200012, 'burglertools', 22, '2005-02-09 10:00:00') /* Key */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (4200012,   1,      16384) /* ItemType - Key */
+     , (4200012,   5,          5) /* EncumbranceVal */
+     , (4200012,   8,          5) /* Mass */
+     , (4200012,  18,          1) /* UiEffects - Magical */
+     , (4200012,  16,    2097160) /* ItemUseable - SourceContainedTargetRemote */
+     , (4200012,  19,        300) /* Value */
+     , (4200012,  33,         -1) /* Bonded - Slippery */
+     , (4200012,  91,          5) /* MaxStructure */
+     , (4200012,  92,          5) /* Structure */
+     , (4200012,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (4200012,  94,        640) /* TargetType - LockableMagicTarget */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (4200012,  22, True ) /* Inscribable */
+     , (4200012,  23, True ) /* DestroyOnSell */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (4200012,   1, 'Burgler''s Tool') /* Name */
+     , (4200012,  13, '_bohemund''s_magic_key_') /* KeyCode */
+     , (4200012,  14, 'Use this key to unlock anything.') /* Use */
+     , (4200012,  15, 'This tool is the go to Burgler Item, it seem''s to pop even the most difficult locks') /* ShortDesc */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (4200012,   1, 0x02000160) /* Setup */
+     , (4200012,   3, 0x20000014) /* SoundTable */
+     , (4200012,   8, 0x06001F98) /* Icon */
+     , (4200012,  22, 0x3400002B) /* PhysicsEffectTable */
+     , (4200012,  52,  100689403) /* IconUnderlay */;
