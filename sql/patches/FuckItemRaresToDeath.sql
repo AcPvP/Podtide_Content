c8bc62f2295ec99ea1344e8270bfb2afb363d60a
AcPvP <89650671+AcPvP@users.noreply.github.com>
2022-05-10 16:11:12 -0400

    Create FuckItemRaresToDeath.sql

---
+++ sql/patches/FuckItemRaresToDeath.sql
@@ -0,0 +1,19 @@
+UPDATE ace_world.spell
+SET stat_Mod_Val = 0
+WHERE NAME IN 
+(
+  'Prodigal Acid Bane',
+  'Prodigal Blade Bane',
+  'Prodigal Blood Drinker',
+  'Prodigal Bludgeon Bane',
+  'Prodigal Defender',
+  'Prodigal Flame Bane',
+  'Prodigal Frost Bane',
+  'Prodigal Heart Seeker',
+  'Prodigal Hermetic Link',
+  'Prodigal Impenetrability',
+  'Prodigal Lightning Bane',
+  'Prodigal Piercing Bane',
+  'Prodigal Spirit Drinker',
+  'Prodigal Swift Killer'
+ );
